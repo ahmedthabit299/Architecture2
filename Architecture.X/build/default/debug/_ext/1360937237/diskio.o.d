0f9f15065da1308bcd2c66a10a74eb7266e5ac57
Ahmed Thabit <ahmedthabit299@gmail.com>
2025-12-12 19:37:19 +0200

    test

--- Architecture.X/build/default/debug/_ext/1360937237/diskio.o.d
+++ Architecture.X/build/default/debug/_ext/1360937237/diskio.o.d
@@ -1,13 +1,1 @@
-build/default/debug/_ext/1360937237/diskio.o: ../src/diskio.c ../src/ff.h \
- ../src/ffconf.h ../src/diskio.h ../src/integer.h \
- ../src/sd_spi_lowlevel.h
-
-../src/ff.h:
-
-../src/ffconf.h:
-
-../src/diskio.h:
-
-../src/integer.h:
-
-../src/sd_spi_lowlevel.h:
+build/default/debug/_ext/1360937237/diskio.o: ../src/diskio.c
--- Architecture.X/build/default/debug/_ext/1360937237/ff.o.d
+++ Architecture.X/build/default/debug/_ext/1360937237/ff.o.d
@@ -1,10 +1,1 @@
-build/default/debug/_ext/1360937237/ff.o: ../src/ff.c ../src/ff.h \
- ../src/ffconf.h ../src/diskio.h ../src/integer.h
-
-../src/ff.h:
-
-../src/ffconf.h:
-
-../src/diskio.h:
-
-../src/integer.h:
+build/default/debug/_ext/1360937237/ff.o: ../src/ff.c
